f8ed2d5fdf11097fb98571796128220e52c35083
Alexander Petranov <shasho68.5@gmail.com>
2024-07-26 12:15:06 +0300

    Change mobile menu to use shadcn component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,15 @@ import { Button } from "./ui/button"
 import Link from "next/link"
 import { MenuIcon } from "lucide-react"
 import { X } from "lucide-react"
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  
 
 function Header() {
     const changeVisMenu = () => {
@@ -66,7 +75,7 @@ function Header() {
             Blog<span className="text-indigo-600">Verse</span>
         </div>
         
-        <div id="mobileMenu" className="top-[12%] md:hidden  duration-500 right-[-100%] p-12 bg-white h-full w-3/4 fixed z-20">
+        {/* <div id="mobileMenu" className="top-[12%] md:hidden  duration-500 right-[-100%] p-12 bg-white h-full w-3/4 fixed z-20">
             <div className="flex flex-col space-y-2">
                 <Button onClick={closeMenuCreate} asChild className="bg-white hover:text-white text-black border-2 rounded-lg border-black">
                     <Link href="/allBlogs">All Blogs</Link>
@@ -75,7 +84,9 @@ function Header() {
                     <Link href="/dashboard">LogIn</Link>
                 </Button>
             </div>
-        </div>
+        </div> */}
+
+        
 
         <div className="hidden items-center md:flex space-x-2">
             <Button className="bg-white hover:text-white text-black border-2 rounded-lg border-black">
@@ -87,11 +98,30 @@ function Header() {
             
         </div>
 
-        <div className="flex flex-row items-center md:hidden space-x-2">
+        {/* <div className="flex flex-row items-center md:hidden space-x-2">
             <MenuIcon onClick={changeVisMenu} id="menu" className="text-indigo-600" />
             <X onClick={changeVisClose} id="close" className="hidden" />
-        </div>
-        
+        </div> */}
+        <Sheet>
+        <SheetTrigger className="text-indigo-600"><MenuIcon/></SheetTrigger>
+        <SheetContent>
+            <SheetHeader>
+            <SheetTitle className="text-indigo-600">Menu</SheetTitle>
+            <SheetDescription>
+            </SheetDescription>
+            </SheetHeader>
+            <div className="flex flex-col space-y-5">
+            <Button className="bg-white hover:text-white text-black border-2 rounded-lg border-black">
+                <Link href="/allBlogs">All Blogs</Link>
+            </Button>
+            <Button className="bg-white hover:text-white text-black border-2 rounded-lg border-black">
+                <Link href="/dashboard">LogIn</Link>
+            </Button>
+            </div>
+            
+        </SheetContent>
+    </Sheet>
+
     </div>
   )
 }
